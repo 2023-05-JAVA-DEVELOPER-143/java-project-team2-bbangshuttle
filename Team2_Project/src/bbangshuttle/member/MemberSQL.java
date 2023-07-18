8952fe458a8eb8d968342079c2a63e9d0ba6b3bb
mac <mac@localhost>
2023-07-19 08:21:59 +0900

    ..

--- Team2_Project/src/bbangshuttle/member/MemberSQL.java
+++ Team2_Project/src/bbangshuttle/member/MemberSQL.java
@@ -1,7 +1,7 @@
 package bbangshuttle.member;
 
 public class MemberSQL {
-	public static final String  MEMBER_INSERT="insert into userinfo(userid,password,name,email) values(?,?,?,?)";
+	public static final String MEMBER_INSERT="insert into userinfo(userid,password,name,email) values(?,?,?,?)";
 	public static final String MEMBER_SELECT_BY_ID="select userid,password,name,email from userinfo where userid=?";
 	public static final String MEMBER_REMOVE="delete from userinfo where userid=?";
 	public static final String MEMBER_UPDATE="update userinfo set password=?,name=?,email=? where userid=?";
