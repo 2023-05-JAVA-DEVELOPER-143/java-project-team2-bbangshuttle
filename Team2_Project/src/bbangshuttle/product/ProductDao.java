6b2114c8be4b0b7b6827184811a5ba9c27385bb4
aaronkim92 <aaronkim92@gmail.com>
2023-07-18 17:40:06 +0900

    ..

--- Team2_Project/src/bbangshuttle/product/ProductDao.java
+++ Team2_Project/src/bbangshuttle/product/ProductDao.java
@@ -1,5 +1,9 @@
 package bbangshuttle.product;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import bbangshuttle.common.DataSource;
 
 public class ProductDao {
@@ -10,4 +14,25 @@ public class ProductDao {
 	/*
 	 * 상품번호로 검색.
 	 */
+	public Product findByProductNo(int p_no) throws Exception {
+		Product product= null;
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(ProductSQL.PRODUCT_SELECT_BY_NO);
+		pstmt.setInt(1, p_no);
+		ResultSet rs = pstmt.executeQuery();
+		if(rs.next()) {
+			product = new Product(
+					rs.getInt("p_no"),
+					rs.getString("p_name"), 
+					rs.getString("p_desc"),
+					rs.getString("p_image"), 
+					rs.getInt("p_price"),
+					rs.getInt("p_view_count"),
+					rs.getInt("p_view_count")
+					);
+		}
+		return product;
+	}
+	
+	
 }
