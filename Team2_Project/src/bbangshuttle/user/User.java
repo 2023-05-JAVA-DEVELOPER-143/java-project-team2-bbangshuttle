5a999d7bfcabb756dd531b45932daa11932f5e25
ahiivii <axoxoh48@naver.com>
2023-07-18 17:31:29 +0900

    .

--- Team2_Project/src/bbangshuttle/user/User.java
+++ Team2_Project/src/bbangshuttle/user/User.java
@@ -10,6 +10,7 @@ public class User {
 	private String memberEmail;		// 이메일
 	private String memberAddress;	// 주소
 	private int memberBirth;		// 생년월일
+	private String memberNumber;	// 연락처
 	private Date memberRegdate;		// 가입일
 	private int memberPoint;		// 보유 포인트
 	private String orderDetails;	// 주문내역
@@ -18,130 +19,121 @@ public class User {
 	public User() {
 		
 	}
-	
-	public User(String memberId, String memberPassword, String memberName, String email, String memberAddress,
-			int memberBirth, Date memberRegdate, int memberPoint, String orderDetails, String customerService) {
+
+	public User(String memberId, String memberPassword, String memberName, String memberEmail, String memberAddress,
+			int memberBirth, String memberNumber, Date memberRegdate, int memberPoint, String orderDetails,
+			String customerService) {
 		super();
 		this.memberId = memberId;
 		this.memberPassword = memberPassword;
 		this.memberName = memberName;
-		this.memberEmail = email;
+		this.memberEmail = memberEmail;
 		this.memberAddress = memberAddress;
 		this.memberBirth = memberBirth;
+		this.memberNumber = memberNumber;
 		this.memberRegdate = memberRegdate;
 		this.memberPoint = memberPoint;
 		this.orderDetails = orderDetails;
 		this.customerService = customerService;
 	}
 
-	
-	
 	public String getMemberId() {
 		return memberId;
 	}
-
 
 	public void setMemberId(String memberId) {
 		this.memberId = memberId;
 	}
 
-
 	public String getMemberPassword() {
 		return memberPassword;
 	}
-
 
 	public void setMemberPassword(String memberPassword) {
 		this.memberPassword = memberPassword;
 	}
 
-
 	public String getMemberName() {
 		return memberName;
 	}
-
 
 	public void setMemberName(String memberName) {
 		this.memberName = memberName;
 	}
 
-
-	public String getEmail() {
+	public String getMemberEmail() {
 		return memberEmail;
 	}
 
-
-	public void setEmail(String email) {
-		this.memberEmail = email;
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
 	}
-
 
 	public String getMemberAddress() {
 		return memberAddress;
 	}
 
-
 	public void setMemberAddress(String memberAddress) {
 		this.memberAddress = memberAddress;
 	}
-
 
 	public int getMemberBirth() {
 		return memberBirth;
 	}
 
-
 	public void setMemberBirth(int memberBirth) {
 		this.memberBirth = memberBirth;
 	}
 
+	public String getMemberNumber() {
+		return memberNumber;
+	}
+
+	public void setMemberNumber(String memberNumber) {
+		this.memberNumber = memberNumber;
+	}
 
 	public Date getMemberRegdate() {
 		return memberRegdate;
 	}
 
-
 	public void setMemberRegdate(Date memberRegdate) {
 		this.memberRegdate = memberRegdate;
 	}
-
 
 	public int getMemberPoint() {
 		return memberPoint;
 	}
 
-
 	public void setMemberPoint(int memberPoint) {
 		this.memberPoint = memberPoint;
 	}
-
 
 	public String getOrderDetails() {
 		return orderDetails;
 	}
 
-
 	public void setOrderDetails(String orderDetails) {
 		this.orderDetails = orderDetails;
 	}
-
 
 	public String getCustomerService() {
 		return customerService;
 	}
 
-
 	public void setCustomerService(String customerService) {
 		this.customerService = customerService;
 	}
 
-	public String toString() {	// alt + shift + s + s
-		return "UserInfo [memberId=" + memberId + ", memberPassword=" + memberPassword + ", memberName=" + memberName
-				+ ", email=" + memberEmail + ", memberAddress=" + memberAddress + ", memberBirth=" + memberBirth
-				+ ", memberRegdate=" + memberRegdate + ", memberPoint=" + memberPoint + ", orderDetails=" + orderDetails
-				+ ", customerService=" + customerService + "]";
+	@Override
+	public String toString() {
+		return "User [memberId=" + memberId + ", memberPassword=" + memberPassword + ", memberName=" + memberName
+				+ ", memberEmail=" + memberEmail + ", memberAddress=" + memberAddress + ", memberBirth=" + memberBirth
+				+ ", memberNumber=" + memberNumber + ", memberRegdate=" + memberRegdate + ", memberPoint=" + memberPoint
+				+ ", orderDetails=" + orderDetails + ", customerService=" + customerService + "]";
 	}
-
+	
+	
 	
 
 
