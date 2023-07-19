18765e36622484c6ffc52eedea8abf6171bbbe94
rhqkf7496 <rhqkf7496@naver.com>
2023-07-19 18:13:37 +0900

    ..

--- Team2_Project/src/bbangshuttle/member/membert_select_update_delete.sql
+++ Team2_Project/src/bbangshuttle/member/membert_select_update_delete.sql
@@ -1,18 +1,6 @@
 -- delete
 delete from userinfo where member_id='kimshuttle11';
 
--- insert
-insert into userinfo(member_id,member_password,member_name,member_email,member_address,member_birth,member_number,member_regdate,member_point) 
-       values('kimshuttle11','1111','김셔틀','kimshuttle11@gmail.com','서울시 방배동','1995/01/01','010-1111-1111',sysdate,0);
-insert into userinfo(member_id,member_password,member_name,member_email,member_address,member_birth,member_number,member_regdate,member_point)
-       values('leeshuttle22','2222','이셔틀','leeshuttle22@gmail.com','서울시 아현동','1997/01/01','010-2222-2222',sysdate,0);
-insert into userinfo(member_id,member_password,member_name,member_email,member_address,member_birth,member_number,member_regdate,member_point)
-	   values('parkshuttle33','3333','박셔틀','parkshuttle33@gmail.com','서울시 대방동','1993/01/01','010-3333-3333',sysdate,0);
-insert into userinfo(member_id,member_password,member_name,member_email,member_address,member_birth,member_number,member_regdate,member_point)
-       values('choishuttle44','4444','최셔틀','choishuttle44@gmail.com','서울시 상계동','1999/01/01','010-4444-4444',sysdate,0);
-insert into userinfo(member_id,member_password,member_name,member_email,member_address,member_birth,member_number,member_regdate,member_point)
-       values('hanshuttle155','5555','한셔틀','hanshuttle155@gmail.com','서울시 혜화동','1992/01/01','010-5555-5555',sysdate,0);
-
 -- update
 update userinfo set 
     member_password='1234',
@@ -23,7 +11,7 @@ update userinfo set
     member_number='010-xxxx-xxxx',
     member_regdate=sysdate,
     member_point=0
-where memberId='kimshuttle11';
+where member_id='kimshuttle11';
 
 
 -- selete pk
@@ -36,7 +24,7 @@ select member_id,
        member_number, 
        member_regdate, 
        member_point
-from userinfo where memberId = 'parkshuttle33';
+from userinfo where member_id = 'parkshuttle33';
 
 -- select all
 select member_id, 
