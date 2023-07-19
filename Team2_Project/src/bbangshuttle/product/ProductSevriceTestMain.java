43151ac03dbafcf90a1fae82ebd4ddb45709ce8c
yskr7171 <yskr7171@gmail.com>
2023-07-19 11:29:04 +0900

    Merge branch 'main' of https://github.com/2023-05-JAVA-DEVELOPER-143/java-project-team2-bbbb.git into main

--- Team2_Project/src/bbangshuttle/product/ProductSevriceTestMain.java
+++ Team2_Project/src/bbangshuttle/product/ProductSevriceTestMain.java
@@ -6,6 +6,20 @@ public class ProductSevriceTestMain {
 		
 		ProductSevrice productSevrice = new ProductSevrice();
 		
+		System.out.println(productSevrice.ProductFindByNo(10));
+		System.out.println("------------------------------");
+		
 		System.out.println(productSevrice.ProductFindByAll());
+		
+		System.out.println("------------------------------");
+		
+		System.out.println(productSevrice.ProductFindByKetword("촉촉한쵹호칩"));
+		
+		System.out.println("------------------------------");
+		System.out.println(productSevrice.productCountUpdate(10));
+
+		System.out.println("------------------------------");
+
+	
 	}
 }
