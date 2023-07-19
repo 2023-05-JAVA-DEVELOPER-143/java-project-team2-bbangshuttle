3fd26618cf54d90bdc41a6ea3ea00ff6fcad3255
yskr7171 <yskr7171@gmail.com>
2023-07-19 10:41:48 +0900

    프로덕트 서비스 커밋2

---
+++ Team2_Project/src/bbangshuttle/product/ProductSevriceTestMain.java
@@ -0,0 +1,11 @@
+package bbangshuttle.product;
+
+public class ProductSevriceTestMain {
+	
+	public static void main(String[] args)throws Exception{
+		
+		ProductSevrice productSevrice = new ProductSevrice();
+		
+		System.out.println(productSevrice.ProductFindByAll());
+	}
+}
