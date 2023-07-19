687b177b4a129ad81e0e7ced7279a3aa65dc2311
hungrydog3366 <hungrydog3366@gmail.com>
2023-07-19 18:20:24 +0900

    Merge branch 'main' of https://github.com/2023-05-JAVA-DEVELOPER-143/java-project-team2-bbangshuttle.git into main

--- Team2_Project/src/bbangshuttle/member/memberDao.java
+++ Team2_Project/src/bbangshuttle/member/memberDao.java
@@ -10,45 +10,62 @@ public class memberDao {
 	public memberDao() throws Exception {
 		dataSource = new DataSource();
 	}
-	public int insert(Member user) throws Exception{
+	// 회원추가
+	public int insert(Member member) throws Exception{
 		Connection con=dataSource.getConnection();
 		PreparedStatement pstmt=con.prepareStatement(MemberSQL.MEMBER_INSERT);
-		pstmt.setString(1, user.getMemberId());
-		pstmt.setString(2, user.getMemberPassword());
-		pstmt.setString(3, user.getMemberName());
-		pstmt.setString(4, user.getMemberEmail());
-		pstmt.setString(5, user.getMemberAddress());
-		pstmt.setString(6, user.getMemberNumber());
+		pstmt.setString(1, member.getMemberId());
+		pstmt.setString(2, member.getMemberPassword());
+		pstmt.setString(3, member.getMemberName());
+		pstmt.setString(4, member.getMemberEmail());
+		pstmt.setString(5, member.getMemberAddress());
+		pstmt.setString(6, member.getMemberNumber());
 		int rowCount=pstmt.executeUpdate();
 		pstmt.close();
 		con.close();
 		return rowCount;
 	}	
-	
-	public int update(Member user) throws Exception{
+	// 회원정보갱신
+	public int update(Member member) throws Exception{
 		Connection con=dataSource.getConnection();
 		PreparedStatement pstmt=con.prepareStatement(MemberSQL.MEMBER_UPDATE);
-		pstmt.setString(1, user.getMemberName());
-		pstmt.setString(2, user.getMemberAddress());
-		pstmt.setString(3, user.getMemberEmail());
-		pstmt.setString(4, user.getMemberNumber());
+		pstmt.setString(1, member.getMemberName());
+		pstmt.setString(2, member.getMemberAddress());
+		pstmt.setString(3, member.getMemberEmail());
+		pstmt.setString(4, member.getMemberNumber());
 		
 		int rowCount=pstmt.executeUpdate();
 		pstmt.close();
 		con.close();
 		return rowCount;
 	}
-	
-	public int delete(String m_Id) throws Exception{
-		Connection con=dataSource.getConnection();
-		PreparedStatement pstmt=con.prepareStatement(MemberSQL.MEMBER_REMOVE);
-		pstmt.setString(1, m_Id);
-		int rowCount=pstmt.executeUpdate();
-		pstmt.close();
-		con.close();
-		return rowCount;
+	// 회원삭제
+	public int delete(String memberId) throws Exception {
+	    Connection con = dataSource.getConnection();
+	    PreparedStatement pstmt = con.prepareStatement(MemberSQL.MEMBER_REMOVE);
+	    pstmt.setString(1, memberId);
+	    int rowCount = pstmt.executeUpdate();
+	    pstmt.close();
+	    con.close();
+	    return rowCount;
 	}
-	
+	public Member findById(String memberId)throws Exception {
+		Member member=null;
+		Connection con=dataSource.getConnection();
+		PreparedStatement pstmt=con.prepareStatement(MemberSQL.MEMBER_SELECT_BY_ID);
+		pstmt.setString(1, memberId);
+		ResultSet rs = pstmt.executeQuery();
+		if(rs.next()) {
+			member=new Member(
+					rs.getString("memberId"),
+					rs.getString("memberPw"), 
+					rs.getString("memberName"), 
+					rs.getString("memberAddress"),
+					rs.getString("memberEmail"),
+					rs.getString("memberNumber"));
+		}
+		return member;
+	}
 	
 	
 }
