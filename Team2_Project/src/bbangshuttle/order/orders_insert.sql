11c9f90c33e5fe95939c592074e76cff806ea219
harucanana <harucanana@gmail.com>
2023-07-20 08:41:51 +0900

    0720 수정완

--- Team2_Project/src/bbangshuttle/order/orders_insert.sql
+++ Team2_Project/src/bbangshuttle/order/orders_insert.sql
@@ -1,44 +1,27 @@
 /**********************orders insert************************/
-/*
-이름         널?       유형            
----------- -------- ------------- 
-O_NO       NOT NULL NUMBER(10)    
-O_DATE              DATE          
-O_PRICE             NUMBER(10)    
-O_DESC              VARCHAR2(100) 
-MEMBER_ID           VARCHAR2(10)  
-*/
--- ORDER_INSERT
-insert into orders(o_no,o_date,o_price,o_desc,member_id) 
-VALUES (orders_o_no_SEQ.nextval, sysdate, 12000, '촉촉한쵹호칩', 'kimshuttle11');
 
-insert into orders(o_no,o_date,o_price,o_desc,member_id) 
-VALUES (orders_o_no_SEQ.nextval, sysdate, 13000, '파리도좋아하는 바게뜨', 'leeshuttle22');
+insert into orders(o_no,o_date,o_price,o_desc,member_id) VALUES (orders_o_no_SEQ.nextval, sysdate-1, 44000, '촉촉한쵹호칩 외 2종', 'kimshuttle11');
+insert into order_item(oi_no, oi_qty, o_no, p_no) VALUES(order_item_oi_no_SEQ.nextval, 1, orders_o_no_SEQ.currval, 1); 
+insert into order_item(oi_no, oi_qty, o_no, p_no) VALUES(order_item_oi_no_SEQ.nextval, 1, orders_o_no_SEQ.currval, 3);
+insert into order_item(oi_no, oi_qty, o_no, p_no) VALUES(order_item_oi_no_SEQ.nextval, 1, orders_o_no_SEQ.currval, 4);
 
-insert into orders(o_no,o_date,o_price,o_desc,member_id) 
-VALUES (orders_o_no_SEQ.nextval, sysdate, 5000, '따뜻한 아이스 아메리카노', 'parkshuttle33');
+insert into orders(o_no,o_date,o_price,o_desc,member_id) VALUES (orders_o_no_SEQ.nextval, sysdate, 112000, '파리도좋아하는 바게뜨 외1종', 'leeshuttle22');
+insert into order_item(oi_no, oi_qty, o_no, p_no) VALUES(order_item_oi_no_SEQ.nextval, 1, orders_o_no_SEQ.currval, 2);
+insert into order_item(oi_no, oi_qty, o_no, p_no) VALUES(order_item_oi_no_SEQ.nextval, 1, orders_o_no_SEQ.currval, 8);
 
-insert into orders(o_no,o_date,o_price,o_desc,member_id) 
-VALUES (orders_o_no_SEQ.nextval, sysdate, 19000, '이퇄~리안 피자빵', 'choishuttle44');
+insert into orders(o_no,o_date,o_price,o_desc,member_id) VALUES (orders_o_no_SEQ.nextval, sysdate-2, 5000, '따뜻한 아이스 아메리카노 외0종', 'parkshuttle33');
+insert into order_item(oi_no, oi_qty, o_no, p_no) VALUES(order_item_oi_no_SEQ.nextval, 1, orders_o_no_SEQ.currval, 9);
 
-insert into orders(o_no,o_date,o_price,o_desc,member_id) 
-VALUES (orders_o_no_SEQ.nextval, sysdate, 7000, '카라멜레온마끼아토', 'hanshuttle155');
+insert into orders(o_no,o_date,o_price,o_desc,member_id) VALUES (orders_o_no_SEQ.nextval, sysdate, 37000, '이퇄~리안 피자빵 외 2종', 'choishuttle44');
+insert into order_item(oi_no, oi_qty, o_no, p_no) VALUES(order_item_oi_no_SEQ.nextval, 1, orders_o_no_SEQ.currval, 5);
+insert into order_item(oi_no, oi_qty, o_no, p_no) VALUES(order_item_oi_no_SEQ.nextval, 1, orders_o_no_SEQ.currval, 6);
+insert into order_item(oi_no, oi_qty, o_no, p_no) VALUES(order_item_oi_no_SEQ.nextval, 1, orders_o_no_SEQ.currval, 7);
 
-/****************************************************************/
-/*
-이름     널?       유형         
------- -------- ---------- 
-OI_NO  NOT NULL NUMBER(30) 번호
-OI_QTY          NUMBER(30) 수량
-O_NO            NUMBER(30) 주문번호
-P_NO            NUMBER(30) 상품번호
-*/
+insert into orders(o_no,o_date,o_price,o_desc,member_id) VALUES (orders_o_no_SEQ.nextval, sysdate, 7000, '카라멜레온마끼아토', 'hanshuttle155');
 
--- ORDERITEM_INSERT
-insert into order_item(oi_no, oi_qty, o_no, p_no) 
-VALUES(order_item_oi_no_SEQ.nextval, 1, orders_o_no_SEQ.currval, 13);
 
-insert into order_item(oi_no, oi_qty, o_no, p_no) 
-VALUES(order_item_oi_no_SEQ.nextval, 1, orders_o_no_SEQ.currval, 1);
 
-/****************************************************************/
+
+select * from orders;
+
+commit;
--- Team2_Project/src/bbangshuttle/order/orders_select_update_delete.sql
+++ Team2_Project/src/bbangshuttle/order/orders_select_update_delete.sql
@@ -1,45 +1,3 @@
-/**********************orders insert************************/
-/*
-이름         널?       유형            
----------- -------- ------------- 
-O_NO       NOT NULL NUMBER(10)    
-O_DATE              DATE          
-O_PRICE             NUMBER(10)    
-O_DESC              VARCHAR2(100) 
-MEMBER_ID           VARCHAR2(10)  
-*/
--- ORDER_INSERT
-insert into orders(o_no,o_date,o_price,o_desc,member_id) 
-VALUES (orders_o_no_SEQ.nextval, sysdate, 12000, '촉촉한쵹호칩', 'kimshuttle11');
-
-insert into orders(o_no,o_date,o_price,o_desc,member_id) 
-VALUES (orders_o_no_SEQ.nextval, sysdate, 13000, '파리도좋아하는 바게뜨', 'leeshuttle22');
-
-insert into orders(o_no,o_date,o_price,o_desc,member_id) 
-VALUES (orders_o_no_SEQ.nextval, sysdate, 5000, '따뜻한 아이스 아메리카노', 'parkshuttle33');
-
-insert into orders(o_no,o_date,o_price,o_desc,member_id) 
-VALUES (orders_o_no_SEQ.nextval, sysdate, 19000, '이퇄~리안 피자빵', 'choishuttle44');
-
-insert into orders(o_no,o_date,o_price,o_desc,member_id) 
-VALUES (orders_o_no_SEQ.nextval, sysdate, 7000, '카라멜레온마끼아토', 'hanshuttle155');
-
-/****************************************************************/
-/*
-이름     널?       유형         
------- -------- ---------- 
-OI_NO  NOT NULL NUMBER(30) 번호
-OI_QTY          NUMBER(30) 수량
-O_NO            NUMBER(30) 주문번호
-P_NO            NUMBER(30) 상품번호
-*/
-
--- ORDERITEM_INSERT
-insert into order_item(oi_no, oi_qty, o_no, p_no) 
-VALUES(order_item_oi_no_SEQ.nextval, 1, orders_o_no_SEQ.currval, 13); 
-
-insert into order_item(oi_no, oi_qty, o_no, p_no) 
-VALUES(order_item_oi_no_SEQ.nextval, 1, orders_o_no_SEQ.currval, 1); 
 
 /**********************orders select************************/
 --1. 멤버 한사람의(kimshuttle11) 주문전체목록
@@ -57,33 +15,12 @@ where o.member_id = 'hanshuttle155' and o.o_no = 5;
 
 /**********************orders delete************************/
 --1. 주문한개삭제(주문1개삭제,주문상세삭제)
-delete from order_item where o_no = 1;
-delete from orders where o_no = 8;
+delete from order_item where o_no = 5;
+delete from orders where o_no = 7;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+--2. 멤버 한사람의(leeshuttle22) 주문내역 전체삭제
+delete from order_item where o_no in(select o_no from orders where member_id='leeshuttle22');
+delete from orders where member_id = 'leeshuttle22';
 
 
 
