cead53e4e54bf913746b3575eaa7a45932e03237
hungrydog3366 <hungrydog3366@gmail.com>
2023-07-21 08:42:46 +0900

    서비스테스트 수정 출력 - 확인

--- Team2_Project/src/bbangshuttle/member/MemberServiceTest.java
+++ Team2_Project/src/bbangshuttle/member/MemberServiceTest.java
@@ -6,8 +6,9 @@ public class MemberServiceTest {
 	
 	public static void main(String[] args) throws Exception {
 		MemberService memberService = new MemberService();
+		
 		//1.회원가입
-		/*
+		
 		System.out.println("1.회원가입");
 		//String memberId, String memberPassword, String memberName, String memberEmail, String memberAddress, String memberBirth, String memberNumber, Date memberRegdate, int memberPoint
 		int rowCount = memberService.addMember(new Member("sdfsadf", "asdfasdf", "ahffk", "adsfsf@nasdfas", "adsfasf", "1999/09/09", "010-0101-0101", null, 0));
@@ -20,25 +21,14 @@ public class MemberServiceTest {
 		//3.회원아이디중복체크
 		System.out.println("3.회원아이디중복체크");
         String testId1 = "sdfsadf";
-<<<<<<< HEAD
-        String testId2 = "iwantgohome";
+        // String testId2 = "iwantgohome";
         boolean isDuplicated = memberService.isDuplicatedId(testId1);
-        boolean isDuplicated = memberService.isDuplicatedId(testId2);
+        // boolean isDuplicated = memberService.isDuplicatedId(testId2);
         System.out.println(">>>> ID '" + testId1 + "' is " + (isDuplicated ? "중복" : "가입가능"));
-        System.out.println(">>>> ID '" + testId2 + "' is " + (isDuplicated ? "중복" : "가입가능"));
+        // System.out.println(">>>> ID '" + testId2 + "' is " + (isDuplicated ? "중복" : "가입가능"));
 		
         
         //4.회원상세보기
-		System.out.println("회원 상세보기 : " + memberService.memberDetail("asdfasdf"));
-		
-       // String testId2 = "iwantgohome";
-        boolean isDuplicated = memberService.isDuplicatedId(testId1);
-       // boolean isDuplicated = memberService.isDuplicatedId(testId2);
-        System.out.println(">>>> ID '" + testId1 + "' is " + (isDuplicated ? "중복" : "가입가능"));
-       // System.out.println(">>>> ID '" + testId2 + "' is " + (isDuplicated ? "중복" : "가입가능"));
-        *
-        */
-		//4.회원상세보기
 		System.out.println("4.회원상세보기");
 		String memberPassword = "asdfasdf";
 		Member member = memberService.memberDetail(memberPassword);
