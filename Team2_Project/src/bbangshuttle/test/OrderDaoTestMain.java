1723def6be05679db0cabc58228f1b695b5c1ce1
harucanana <harucanana@gmail.com>
2023-07-20 17:29:39 +0900

    ..

--- Team2_Project/src/bbangshuttle/test/OrderDaoTestMain.java
+++ Team2_Project/src/bbangshuttle/test/OrderDaoTestMain.java
@@ -11,11 +11,11 @@ public class OrderDaoTestMain {
 		
 		//System.out.println(orderDao.deleteByMemberId("hanshuttle155"));
 		
-		//System.out.println(orderDao.findByOrderNo(1));
+		System.out.println(orderDao.findByOrderNo(1));
 		
 		//System.out.println(orderDao.findOrderByMemberId("choishuttle44"));
 		
-		System.out.println(orderDao.findOrderWithOrderItemMemberId("kimshuttle11"));
+		//System.out.println(orderDao.findOrderWithOrderItemMemberId("kimshuttle11"));
 		
 		
 		
