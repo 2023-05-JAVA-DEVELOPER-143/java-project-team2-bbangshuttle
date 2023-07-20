f377b02fd825c564c9893aa317503829285e4eb0
yskr7171 <yskr7171@gmail.com>
2023-07-20 15:37:22 +0900

    살려주세요

--- Team2_Project/src/bbangshuttle/cart/CartDaoTestMain.java
+++ Team2_Project/src/bbangshuttle/cart/CartDaoTestMain.java
@@ -5,6 +5,7 @@ public class CartDaoTestMain  {
 	public static void main(String[] args)throws Exception {
 		CartDao cartDao = new CartDao();
 		System.out.println("회원님 장바구니");
-		System.out.println(cartDao.cartSelectNo(1, "kimshuttle11"));
+		System.out.println
+		(cartDao.cartSelectNo(1, "kimshuttle11"));
 	}
 }
