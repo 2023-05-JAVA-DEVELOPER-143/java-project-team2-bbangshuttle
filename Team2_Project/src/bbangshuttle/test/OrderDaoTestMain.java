d68b1dbd1f6c399544b94cd4114854062cf1397b
rhqkf7496 <rhqkf7496@naver.com>
2023-07-20 17:34:43 +0900

    order쪽 dao랑 dao 테스트 올려봅니다~

--- Team2_Project/src/bbangshuttle/test/OrderDaoTestMain.java
+++ Team2_Project/src/bbangshuttle/test/OrderDaoTestMain.java
@@ -7,17 +7,15 @@ public class OrderDaoTestMain {
 	public static void main(String[] args) throws Exception{
 		
 		OrderDao orderDao = new OrderDao();
-		System.out.println(orderDao.findOrderByMemberId("leeshuttle22"));
-		
 		System.out.println(orderDao.deleteByMemberId("hanshuttle155"));
 		
+		System.out.println(orderDao.daleteByOrderNo(5));
+		
+		System.out.println(orderDao.findOrderByMemberId("leeshuttle22"));
+		
+		System.out.println(orderDao.findOrderWithOrderItemMemberId("kimshuttle11"));
+		
 		System.out.println(orderDao.findByOrderNo(1));
-		
-		System.out.println(orderDao.findOrderByMemberId("choishuttle44"));
-		
-		//System.out.println(orderDao.findOrderWithOrderItemMemberId("kimshuttle11"));
-		
-		
 		
 		
 	}
