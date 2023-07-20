7e6ed818354ac37c0329a4663ffda560ac2e1318
yskr7171 <yskr7171@gmail.com>
2023-07-20 17:56:17 +0900

    ..

--- Team2_Project/src/bbangshuttle/cart/CartSQL.java
+++ Team2_Project/src/bbangshuttle/cart/CartSQL.java
@@ -10,9 +10,9 @@ public class CartSQL {
 	//" "회원 장바구니 상품 전체 읽기
 	public static final String CART_MEMBERID_SELECT_BY_ALL = "select * from cart c join product p on c.p_no=p.p_no where c.member_id =?";
 	//" "회원 장바구니 " "품목 하나 업데이트
-	public static final String CART_MEMBERID_UPDATE = "update cart set cart_qty = cart_qty+10 where member_id = ?  and p_no = ? ";
+	public static final String CART_MEMBERID_UPDATE = "update cart set cart_qty= ? where member_id = ?  and p_no = ? ";
 	//" "회원 장바구니 품목 전체 변경
-	public static final String CART_MEMBERID_UPDATE_ALL = "update cart set cart_qty = cart_qty+? where member_id =?";
+	public static final String CART_MEMBERID_UPDATE_ALL = "update cart set cart_qty = ? where member_id =?";
 	//" "회원 장바구니 ""품목 선택 삭제
 	public static final String CART_MEMBERID_DELETE_BY_P_NO="delete from cart where member_id=? and p_no=?";
 	//" " 회원 장바구니 전체 삭제
