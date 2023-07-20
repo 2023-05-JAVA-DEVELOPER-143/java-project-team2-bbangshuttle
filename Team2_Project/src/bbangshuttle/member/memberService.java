4ef818401edc4260551de3dd1c3f75c77ea5e577
hungrydog3366 <hungrydog3366@gmail.com>
2023-07-20 13:11:18 +0900

    ..

--- Team2_Project/src/bbangshuttle/member/memberService.java
+++ Team2_Project/src/bbangshuttle/member/memberService.java
@@ -1,9 +1,17 @@
 package bbangshuttle.member;
 
-public class memberService {
+import java.util.List;
 
+public class MemberService {
+	private MemberDao memberDao;
+	public MemberService() throws Exception{
+		memberDao=new MemberDao();
+	}
 	
 	// 회원가입
+	public boolean addMember(Member newMember) throws Exception{
+		boolean isSuccess=false;
+	}
 	// 아이디 중복 체크
 	
 	// 로그인
