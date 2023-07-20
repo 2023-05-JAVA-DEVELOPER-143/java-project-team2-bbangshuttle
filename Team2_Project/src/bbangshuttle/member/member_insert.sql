13ae8fe6c948fd1cf224af89d8716dd820655898
rhqkf7496 <rhqkf7496@naver.com>
2023-07-20 10:25:39 +0900

    ..

--- Team2_Project/src/bbangshuttle/member/member_insert.sql
+++ Team2_Project/src/bbangshuttle/member/member_insert.sql
@@ -3,3 +3,5 @@ insert into userinfo(member_id,member_password,member_name,member_email,member_a
 insert into userinfo(member_id,member_password,member_name,member_email,member_address,member_birth,member_number,member_regdate,member_point) values('parkshuttle33','3333','박셔틀','parkshuttle33@gmail.com','서울시 대방동','1993/01/01','010-3333-3333',sysdate,0);
 insert into userinfo(member_id,member_password,member_name,member_email,member_address,member_birth,member_number,member_regdate,member_point) values('choishuttle44','4444','최셔틀','choishuttle44@gmail.com','서울시 상계동','1999/01/01','010-4444-4444',sysdate,0);
 insert into userinfo(member_id,member_password,member_name,member_email,member_address,member_birth,member_number,member_regdate,member_point) values('hanshuttle155','5555','한셔틀','hanshuttle155@gmail.com','서울시 혜화동','1992/01/01','010-5555-5555',sysdate,0);
+
+commit;
