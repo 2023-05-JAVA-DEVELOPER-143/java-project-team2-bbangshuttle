9d7f787ccbf2d6bfd265e4ab7f0c6add804aa102
aaronkim92 <aaronkim92@gmail.com>
2023-07-20 09:34:55 +0900

    order by desc기능추가

--- Team2_Project/src/bbangshuttle/product/ProductSQL.java
+++ Team2_Project/src/bbangshuttle/product/ProductSQL.java
@@ -1,7 +1,7 @@
 package bbangshuttle.product;
 
 public class ProductSQL {
-	public final static String PRODUCT_SELECT_ALL="select * from product";
+	public final static String PRODUCT_SELECT_ALL="select * from product order by p_view_count desc";
 	public final static String PRODUCT_SELECT_BY_NO="select * from product where p_no=?";
 	public final static String PRODUCT_SELECT_BY_KEYWORD="select * from product where p_name like '%'||?||'%' or p_desc like '%'||?||'%'";
 	public final static String PRODUCT_UPDATE_COUNT="update product set p_view_count=p_view_count+1 where p_no=?";
