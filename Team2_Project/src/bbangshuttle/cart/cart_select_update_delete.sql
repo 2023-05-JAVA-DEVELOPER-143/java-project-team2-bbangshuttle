c33298611802efea7ff0b03e98a9384dc86a593b
yskr7171 <yskr7171@gmail.com>
2023-07-20 18:37:48 +0900

    카트 테스트 커밋

--- Team2_Project/src/bbangshuttle/cart/cart_select_update_delete.sql
+++ Team2_Project/src/bbangshuttle/cart/cart_select_update_delete.sql
@@ -17,7 +17,7 @@ update cart set cart_qty = cart_qty+10 where member_id ='kimshuttle11'  and p_no
 -- 회원 한개주문 업데이트 2
 update cart set cart_qty =15 where member_id ='leeshuttle22'  and p_no = 1 ;
 -- 회원의 전체주문 업데이트
-update cart set cart_qty = cart_qty+7 where member_id ='kimshuttle11' ;
+update cart set cart_qty =7 where member_id ='leeshuttle22' ;
 
 -- 회원 주문삭제
 delete from cart where member_id='kimshuttle11';
