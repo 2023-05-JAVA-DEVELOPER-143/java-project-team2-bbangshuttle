74c048f6eed37178a6a8f098fcc836e8a3cfda77
ahiivii <axoxoh48@naver.com>
2023-07-20 18:57:04 +0900

    엥

--- Team2_Project/src/bbangshuttle/member/MemberServiceTest.java
+++ Team2_Project/src/bbangshuttle/member/MemberServiceTest.java
@@ -7,6 +7,7 @@ public class MemberServiceTest {
 	public static void main(String[] args) throws Exception {
 		MemberService memberService = new MemberService();
 		//1.회원가입
+		/*
 		System.out.println("1.회원가입");
 		//String memberId, String memberPassword, String memberName, String memberEmail, String memberAddress, String memberBirth, String memberNumber, Date memberRegdate, int memberPoint
 		int rowCount = memberService.addMember(new Member("sdfsadf", "asdfasdf", "ahffk", "adsfsf@nasdfas", "adsfasf", "1999/09/09", "010-0101-0101", null, 0));
@@ -18,13 +19,25 @@ public class MemberServiceTest {
 
 		//3.회원아이디중복체크
 		System.out.println("3.회원아이디중복체크");
-        String testId = "sdfsadf";
-        boolean isDuplicated = memberService.isDuplicatedId(testId);
-        System.out.println(">>>> ID '" + testId + "' is " + (isDuplicated ? "duplicated" : "unique"));
-		//4.회원상세보기
+        String testId1 = "sdfsadf";
+        String testId2 = "iwantgohome";
+        boolean isDuplicated = memberService.isDuplicatedId(testId1);
+        boolean isDuplicated = memberService.isDuplicatedId(testId2);
+        System.out.println(">>>> ID '" + testId1 + "' is " + (isDuplicated ? "중복" : "가입가능"));
+        System.out.println(">>>> ID '" + testId2 + "' is " + (isDuplicated ? "중복" : "가입가능"));
+		
+        
+        //4.회원상세보기
+		System.out.println("회원 상세보기 : " + memberService.memberDetail("asdfasdf"));
+		*/
 		
 		//5.회원수정
+		Member updateMember = memberService.memberDetail("6666");
+		updateMember.setMemberAddress("아이티윌 학원");
+        System.out.println("회원수정 : " + memberService.memberUpdate(updateMember));
+		
 		//6.회원탈퇴
+        
 	}
 	
 	
