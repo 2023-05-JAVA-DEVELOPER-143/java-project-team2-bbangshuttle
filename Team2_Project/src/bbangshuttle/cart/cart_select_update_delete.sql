08c1451b6391a3be79ab6691437e958deaeb2fc6
rhqkf7496 <rhqkf7496@naver.com>
2023-07-20 10:43:42 +0900

    .

--- Team2_Project/src/bbangshuttle/cart/cart_select_update_delete.sql
+++ Team2_Project/src/bbangshuttle/cart/cart_select_update_delete.sql
@@ -1,1 +1,6 @@
-/**********cart insert**********/
+--leeshuttle22 멤버한사람의 카트아이템리스트
+select * from 
+cart c join userinfo u on c.member_id = u.member_id 
+	   join product p on p.p_no = c.p_no where u.member_id = 'leeshuttle22';
+       
+
